1c70c343734277cfcf81e1194bf9999fda7cec40
laurie kemmerer <laurie.kemmerer@gmail.com>
2012-10-09 20:40:11 -0700

    have a test properly failing (once again, js !== ruby)

--- test/orestar_importer_test.js
+++ test/orestar_importer_test.js
@@ -7,12 +7,12 @@ var graph   = dbSupport.graph;
 
 vows.describe('Importer').addBatch({
   'knows what is what': {
-    topic: function() { return new OrestarImporter(testFile, graph) },
-    'can read a csv': function(topic) {
-      topic.rows().should == [
+    topic: function() { return new Importer('./../fixtures/test_file.xls', graph) },
+    'can read an xls': function(topic) {
+      assert.equal(topic.rows(), [
         { id: '15', name: 'PAC Filer Man' },
         { id: '42', name: 'Anna Lisa' }
-      ]
+      ]);
     },
     teardown: function() {
       dbSupport.clear(graph);
